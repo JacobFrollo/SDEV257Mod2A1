fe4dec60487c442ecb13a248fd1e953b8da33a78
JacobFrollo <159093866+JacobFrollo@users.noreply.github.com>
2025-11-10 14:03:38 -0500

    Update Row.js

--- Row.js
+++ Row.js
@@ -3,10 +3,12 @@ import PropTypes from "prop-types";
 import { View } from "react-native";
 import styles from "./styles";
 
+//Styles the rows
 export default function Row({ children }) {
     return <View style={styles.row}>{children}</View>;
 }
 
+//Creates the rows
 Row.propTypes = {
     children: PropTypes.node.isRequired,
 };
