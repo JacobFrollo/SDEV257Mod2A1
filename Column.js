4dff152cc1e17c29fcfb6d6e358f0aec4812431d
JacobFrollo <159093866+JacobFrollo@users.noreply.github.com>
2025-11-10 14:03:16 -0500

    Update Column.js

--- Column.js
+++ Column.js
@@ -3,10 +3,12 @@ import PropTypes from "prop-types";
 import { View } from "react-native";
 import styles from "./styles";
 
+//styles the columns
 export default function Column({ children }) {
     return <View style={styles.column}>{children}</View>;
 }
 
+//Creates the columns
 Column.propTypes = {
     children: PropTypes.node.isRequired,
 };
